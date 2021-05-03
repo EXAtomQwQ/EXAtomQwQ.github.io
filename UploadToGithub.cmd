76ea3f30b56d3fe678df40e4761a9fd67c452dc5
EXAtom <3522542971@qq.com>
2021-05-03 22:17:34 +0800

    Commit to Github - Use Git Upload

---
+++ UploadToGithub.cmd
@@ -0,0 +1,8 @@
+@echo off
+git config --global user.name 'EXAtom'
+git config --global user.email '3522542971@qq.com'
+git config --global --unset http.proxy
+git config --global --unset https.proxy
+git add .
+git commit -m "Commit to Github - Use Git Upload"
+git push -u origin main
